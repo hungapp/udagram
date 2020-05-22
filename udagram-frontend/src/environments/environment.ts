f9924c66b959d7b1e33c4d1fc96924a5278026a2
Hung Nguyen <viethung1996@gmail.com>
2020-05-22 09:11:11 -0400

    fix: Edit deployment configs

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,8 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0',
+  apiHost:
+    'http://a4207b9704430474b9d83446fd1eff34-624138856.us-east-2.elb.amazonaws.com:8080/api/v0',
 };
 
 /*
