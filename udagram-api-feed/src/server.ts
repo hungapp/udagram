814d8f21bd3dd18dd2fc5754902233268916bfd1
Hung Nguyen <viethung1996@gmail.com>
2020-05-22 17:46:18 -0400

    fix: Edit CORS

--- udagram-api-feed/src/server.ts
+++ udagram-api-feed/src/server.ts
@@ -17,7 +17,7 @@ import { FeedItem } from './controllers/v0/feed/models/FeedItem';
 
   //CORS Should be restricted
   app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:8100');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header(
       'Access-Control-Allow-Headers',
       'Origin, X-Requested-With, Content-Type, Accept, Authorization'
--- udagram-api-user/src/server.ts
+++ udagram-api-user/src/server.ts
@@ -18,7 +18,7 @@ import { User } from './controllers/v0/users/models/User';
 
   //CORS Should be restricted
   app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:8100');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header(
       'Access-Control-Allow-Headers',
       'Origin, X-Requested-With, Content-Type, Accept, Authorization'
